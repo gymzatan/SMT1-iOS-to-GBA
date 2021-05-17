85d37f687131a6c3d58e81852062f8cfd9f5f0a0
Ds886 <daniel.ashkenazi@gmail.com>
2021-05-17 21:01:31 +0300

    fix

--- SMT1_create.bat
+++ SMT1_create.bat
@@ -1,25 +1,4 @@
 @ECHO off
-GOTO :MAIN
-
-:: Functions
-:printInstruction
-ECHO USAGE: SMT1_create.bat [path to original rom] [path to iOS rom]
-
-:applyroms
-ECHO Patching GBA rom
-bin\flips.exe --apply "SMT1_1_gba.bps" %orig% "SMT1_1_gba.gba"
-ECHO Patching iOS rom
-bin\flips.exe --apply "SMT1_1_ios.bps" %ios% "SMT1_1_ios.gba"
-GOTO :combineroms
-
-:combineroms
-ECHO Combining roms
-copy /b SMT1_1_gba.gba+SMT1_2_ios.gba SMT1_new.gba
-
-
-
-
-:MAIN
 SET orig=%1
 SET ios=%2
 
@@ -45,3 +24,17 @@ GOTO:EOF
 ECHO Applying patchs
 GOTO :applyroms
 
+:: Functions
+:printInstruction
+ECHO USAGE: SMT1_create.bat [path to original rom] [path to iOS rom]
+
+:applyroms
+ECHO Patching GBA rom
+bin\flips.exe --apply "SMT1_1_gba.bps" %orig% "SMT1_1_gba.gba"
+ECHO Patching iOS rom
+bin\flips.exe --apply "SMT1_1_ios.bps" %ios% "SMT1_1_ios.gba"
+GOTO :combineroms
+
+:combineroms
+ECHO Combining roms
+copy /b SMT1_1_gba.gba+SMT1_2_ios.gba SMT1_new.gba
