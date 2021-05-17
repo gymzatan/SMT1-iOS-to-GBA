924b05f3f1cfa684116b962423857b93c34c07e2
Ds886 <daniel.ashkenazi@gmail.com>
2021-05-18 00:04:47 +0300

    fix filenames

--- SMT1_create.bat
+++ SMT1_create.bat
@@ -21,8 +21,6 @@ CALL :printInstruction
 EXIT /B
 )
 
-ECHO Cleaning old builds
-GOTO clean
 
 ECHO Applying patchs
 GOTO :applyroms
@@ -34,12 +32,18 @@ GOTO :applyroms
 ECHO USAGE: SMT1_create.bat [path to original rom] [path to iOS rom]
 EXIT /B
 
-:clean
-del SMT1_1_gba.gba
-del SMT1_2_ios.gba
-EXIT /B
-
 :applyroms
+ECHO Cleaning old patches
+IF EXIST SMT1_1_gba.gba (
+del SMT1_1_gba.gba
+)
+IF EXIST SMT1_2_ios.gba (
+del SMT1_2_ios.gba
+)
+IF EXIST SMT1_NEW.gba (
+del SMT1_new.gba
+)
+
 ECHO Patching GBA rom
 bin\beat.exe -apply -p "SMT1_1_gba.bps" -o "SMT1_1_gba.gba" %orig%
 ECHO Patching iOS rom
